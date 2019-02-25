40cde56534b7d531ef08b5e3dbd911331479f105
RuksinaPrommee <36035105+RuksinaPrommee@users.noreply.github.com>
2019-02-25 13:08:51 +0700

    commit

--- src/app/jod-detail-announcement/jod-detail-announcement/jod-detail-announcement.component.ts
+++ src/app/jod-detail-announcement/jod-detail-announcement/jod-detail-announcement.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthGuard } from '../../_guards';
+import { User, Role } from '../../_models';
+import { UserService, AuthenticationService } from '../../_services';
 @Component({
   selector: 'app-jod-detail-announcement',
   templateUrl: './jod-detail-announcement.component.html',
@@ -17,9 +20,36 @@ export class JodDetailAnnouncementComponent implements OnInit {
     { name: 'Gender' },
     { name: 'Company' }
   ];
-  constructor() { }
+  currentUser: User;
+  userFromApi: User;
+  constructor(  private router: Router,
+    private userService: UserService,
+    private authenticationService: AuthenticationService,
+    private authGuardService: AuthGuard) {  this.userFromApi = this.currentUser = this.authenticationService.currentUserValue;}
 
   ngOnInit() {
   }
 
+  get isSignin() {
+    if (this.currentUser != null) {
+      return true;
+    }
+    return false;
+  }
+
+  get isUser() {
+    return this.currentUser && this.currentUser.accTypeId === Role.User;
+  }
+
+  get isStaff() {
+    return this.currentUser && this.currentUser.accTypeId === Role.Staff;
+  }
+
+  get isAgent() {
+    return this.currentUser && this.currentUser.accTypeId === Role.Agent;
+  }
+
+  get isAdmin() {
+    return this.currentUser && this.currentUser.accTypeId === Role.Admin;
+  }
 }
