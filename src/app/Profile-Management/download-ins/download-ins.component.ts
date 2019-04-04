f991406c4f43a8e583557ea81f21f524f6a7f5f8
garagetKet <58160651@go.buu.ac.th>
2019-04-04 16:42:12 +0700

    ดึงข้อมูลจากหลังบ้านมาแสดงหน้าบ้าน

--- src/app/Profile-Management/download-ins/download-ins.component.ts
+++ src/app/Profile-Management/download-ins/download-ins.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StudentdataInterface } from '../../_models/stu-data-interface';
 import { StudentDataService } from '../../services/std-ins-form.service';
+import { AuthGuard } from '../../_guards';
+import { User, Role } from '../../_models';
+import { UserService, AuthenticationService } from '../../_services';
 
 @Component({
   selector: 'app-download-ins',
@@ -10,18 +13,45 @@ import { StudentDataService } from '../../services/std-ins-form.service';
 })
 export class DownloadInsComponent implements OnInit {
 
-  std: StudentdataInterface
+  currentUser: User;
+  userFromApi: User;
+  std: any
   
-  constructor(){}//private route: ActivatedRoute, private studentdataService: StudentDataService) { }
-
-  ngOnInit() {
-    // this.getstudentdata()
+  constructor(
+    private route: ActivatedRoute, 
+    private authenticationService: AuthenticationService, 
+    private studentdataService: StudentDataService,
+    private authGuardService: AuthGuard
+  ) { 
+    this.userFromApi = this.currentUser = this.authenticationService.currentUserValue;
   }
 
-  // getstudentdata(stdId: number){
-  //   this.studentdataService.getstddataBystdId(stdId)
+  ngOnInit() {
+    // this.getstudentdata(this.currentUser.userId);
+    this.getstudentdata(this.currentUser.userId);
+    // console.log("std id : ", this.currentUser.userId);
+     //this.getstudentdata()
+  }
+  get isSignin() {
+    if (this.currentUser != null) {
+      return true;
+    }
+    return false;
+  }
 
-  // }
+  get isUser() {
+    return this.currentUser && this.currentUser.userId === Role.User;
+  }
+
+  getstudentdata(stdId: number){
+
+    // console.log("std id : ", stdId);
+    // console.log("std : ", this.userFromApi);
+    this.studentdataService.getstdBystdId(stdId).subscribe((data: {}) => {
+    
+      this.std = data;
+    });
+  }
 
 
 }
--- src/app/services/std-ins-form.service.ts
+++ src/app/services/std-ins-form.service.ts
@@ -1,29 +1,48 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { map } from 'rxjs/operators';
 import { AuthenticationService } from '../_services';
 import { StudentdataInterface } from '../_models/stu-data-interface';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class StudentDataService{
 
     private httpHeaders: HttpHeaders;
+    private httpHeadersRes: HttpHeaders;
     private extractData(res: Response) {
         const body = res;
         return body || {};
     }
     constructor(private http: HttpClient, private authenticationService: AuthenticationService) { 
-        this.httpHeaders = this.authenticationService.gethttpHeadersRes;
+        // this.httpHeaders = this.authenticationService.gethttpHeadersRes;
+        this.httpHeadersRes = this.authenticationService.gethttpHeadersRes;
+        this.httpHeaders = this.authenticationService.gethttpHeaders;
     }
 
-    get(): Observable<StudentdataInterface[]>{
-        return this.http.get<StudentdataInterface[]>(`${this.authenticationService.path_url}/std_printout_ins001`,
-            ({ headers: this.httpHeaders }))
+    errorHander(error: HttpErrorResponse) {
+        // console.log("err status : ",error.status);
+        return Observable.throw(error.message);
     }
 
-    getstddataBystdId(std_id: number): Observable<StudentdataInterface>{
+    getstdBystdId(std_id: number): Observable<StudentdataInterface> {
+        return this.http.get<StudentdataInterface>(`${this.authenticationService.path_url}/std_printout_ins001/${std_id}`,
+            ({ headers: this.httpHeadersRes }));
+    }
+
+    // getstdBystdId2(std_id: number): Observable<StudentdataInterface[]>{
+    //     console.log("test : ", 55555);
+    //     console.log("url : ", `${this.authenticationService.path_url}/std_printout_ins001/${std_id}`);
+    //     return this.http.get<StudentdataInterface[]>(`${this.authenticationService.path_url}/std_printout_ins001/${std_id}`,
+    //         ({ headers: this.httpHeaders })).pipe(map(data => { console.log("req data => ",data);
+    //             return data
+    //         }), catchError(this.errorHander));
+    // }
+
+    getstddataBystdId(std_id: string): Observable<StudentdataInterface>{
         return this.http.get<StudentdataInterface>(`${this.authenticationService.path_url}/std_addform/${std_id}`, 
         ({ headers: this.httpHeaders }))
     }
