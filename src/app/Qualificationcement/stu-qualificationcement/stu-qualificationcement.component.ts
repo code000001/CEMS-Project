2de4f3f03667e1287b02f4a927b91a66a1076e8a
Phakhanan Thongmee <arunsittiwakrai@hotmail.com>
2019-04-30 00:57:38 +0700

    student update fix

--- src/app/Qualificationcement/stu-qualificationcement/stu-qualificationcement.component.ts
+++ src/app/Qualificationcement/stu-qualificationcement/stu-qualificationcement.component.ts
@@ -5,6 +5,8 @@ import { UserService, AuthenticationService } from 'src/app/_services';
 import { AuthGuard } from 'src/app/_guards';
 import { StudentDataService } from 'src/app/services/std-ins-form.service';
 import { StudentdataInterface } from 'src/app/_models/stu-data-interface';
+import { StudentdataInterfaceQualifying } from 'src/app/_models/stu-data-qualifying-interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-stu-qualificationcement',
@@ -15,7 +17,9 @@ export class StuQualificationcementComponent implements OnInit {
   id: number
   currentUser: User;
   userFromApi: User;
-  stdData: StudentdataInterface[];
+  stdInqual: StudentdataInterface[];
+  stdQualified: StudentdataInterface[];
+  stdAppForm: StudentdataInterface[];
 
   constructor(  private router: Router,
     private userService: UserService,
@@ -30,13 +34,125 @@ export class StuQualificationcementComponent implements OnInit {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
       }
     }
+
+    updateToQualifying(id : number){
+      let stdInQualifying: StudentdataInterfaceQualifying = {
+        id: null,
+        stdAccId: id,
+        stdStatusId: 4,
+        stdId: null,
+        stdTestScore: null,
+        stdHrPrepare: null,
+        stdHrConference: null
+      };
+
+      this.studentdataService.putStatusUpdate(stdInQualifying)
+    .subscribe((m) => {
+    },(err)=>{
+      Swal.fire({
+        type: 'warning',
+        title: 'ไม่สามารถบันทึกข้อมูลได้',
+        showConfirmButton: false,
+        timer: 5000
+      })     
+    },()=>{
+      
+        Swal.fire({
+              type: 'success',
+              title: 'บันทึกข้อมูลสำเร็จ',
+              showConfirmButton: false,
+              timer: 1500
+            })     
+            setTimeout(()=> window.location.reload(), 1500)  
+    })
+    }
+
+    updateToPass(id: number){
+      let stdInQualifying: StudentdataInterfaceQualifying = {
+        id: null,
+        stdAccId: id,
+        stdStatusId: 5,
+        stdId: null,
+        stdTestScore: null,
+        stdHrPrepare: null,
+        stdHrConference: null
+      };
+
+      this.studentdataService.putStatusUpdate(stdInQualifying)
+    .subscribe((m) => {
+    },(err)=>{
+      Swal.fire({
+        type: 'warning',
+        title: 'ไม่สามารถบันทึกข้อมูลได้',
+        showConfirmButton: false,
+        timer: 5000
+      })     
+    },()=>{
+      
+        Swal.fire({
+              type: 'success',
+              title: 'บันทึกข้อมูลสำเร็จ',
+              showConfirmButton: false,
+              timer: 1500
+            })
+            setTimeout(()=> window.location.reload(), 1500)     
+            
+    })
+
+    }
+
+    updateToFail(id: number){
+      let stdInQualifying: StudentdataInterfaceQualifying = {
+        id: null,
+        stdAccId: id,
+        stdStatusId: 2,
+        stdId: null,
+        stdTestScore: null,
+        stdHrPrepare: null,
+        stdHrConference: null
+      };
+
+      this.studentdataService.putStatusUpdate(stdInQualifying)
+    .subscribe((m) => {
+    },(err)=>{
+      Swal.fire({
+        type: 'warning',
+        title: 'ไม่สามารถบันทึกข้อมูลได้',
+        showConfirmButton: false,
+        timer: 5000
+      })     
+    },()=>{
+      
+        Swal.fire({
+              type: 'success',
+              title: 'บันทึกข้อมูลสำเร็จ',
+              showConfirmButton: false,
+              timer: 1500
+            })     
+            setTimeout(()=> window.location.reload(), 1500)  
+    })
+    }
   
     getOrganization(): void {
-       this.studentdataService.getStudentDataQualification()
+       this.studentdataService.getStudentDataQualification(3)
          .subscribe(
-           resultData => { this.stdData = resultData; },
+           resultData => { 
+             this.stdAppForm = resultData;
+           },
         )
-        console.log(this.stdData);
+        this.studentdataService.getStudentDataQualification(4)
+         .subscribe(
+           resultData => { 
+             this.stdInqual = resultData;
+           },
+        )
+        this.studentdataService.getStudentDataQualification(5)
+         .subscribe(
+           resultData => { 
+             this.stdQualified = resultData;
+           },
+        )
+      
     }
   
   get isSignin() {
